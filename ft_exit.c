6a6b9bfab0db7ad026aea4333163d578f926d0c1
Leo Broue <lebroue@z2r12p4.42lyon.fr>
2025-05-15 16:38:47 +0200

    The exit Function : Overflow from long long int min and max has been successfuly modified. Still to norme the 'isnumber function' for next time

--- ft_exit.c
+++ ft_exit.c
@@ -1,15 +1,16 @@
 
 #include "builtins.h"
 
-long long    ft_atoll(const char *str)
-{
-    int            i;
-    int            sign;
-    long long int   result;
 
-    i = 0;
-    sign = 1;
-    result = 0;
+#define INT64_MAX_STR "9223372036854775807"
+#define INT64_MIN_STR "9223372036854775808"
+
+long long ft_atoll(const char *str)
+{
+    int i = 0;
+    int sign = 1;
+    long long result = 0;
+
     while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
         i++;
     if (str[i] == '-' || str[i] == '+')
@@ -20,57 +21,84 @@ long long    ft_atoll(const char *str)
     }
     while (str[i] >= '0' && str[i] <= '9')
     {
-        result = 10 * result + (str[i] - 48);
+        result = 10 * result + (str[i] - '0');
         i++;
     }
-    return (result * (long long)sign);
+    return (result * sign);
+}
+bool check_isdigit(const char *nb) /// verifie que c'est bien un digit 
+{
+
+	int j;
+	j = 0;
+	while(nb[j]) 
+	{
+		if (!ft_isdigit(nb[j]))
+			return false;
+		j++;
+	}
+	return true;
+}
+bool isnumber(const char *str) //////// A NORMER
+{
+    int i;
+    int sign;
+    const char *nb;
+
+    i = 0;
+    sign = 1;
+
+    if (!str || !*str) //// securiter de str NULL
+        return false;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)) /// skip  des white spaces
+        i++;
+    if (str[i] == '-' || str[i] == '+') /// comme dans atoll si + ou - ... 
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    nb = &str[i];
+    while (*nb == '0') ///// gere les 0 au debut du nombre et les skip
+        nb++;
+    int nb_count;
+	nb_count = 0;
+    while (ft_isdigit(nb[nb_count]))//// calcule la len de nb
+        nb_count++;
+    if (nb_count == 0) /// si la len de nb vaut 0, alors return false
+        return (false);
+    if (nb_count > 19) /// si elle est supprerieur a 19 alors elle overflow
+        return (false);
+    if (nb_count == 19) /// si elle est egale a 19 alors, on verifie que le nombre noverflow pas en int max et int min 
+    {
+        if (sign == 1 && ft_strncmp(nb, INT64_MAX_STR, 19) > 0) /// si le int et positif alors, on vefie que elle noverflow pas en la comparant au int64 max
+            return (false);
+        if (sign == -1 && ft_strncmp(nb, INT64_MIN_STR, 19) > 0)/// si le int et negatif alors, on vefie que elle noverflow pas en la comparant au int64 min
+            return (false);
+    }
+	return (check_isdigit(nb));
 }
 
-static bool isnumber(char *str) /// Condition impossible car long long est deja en INT64 MAX ou INT64 MIN
+int ft_exit(char **args)
 {
-	int i = 0;
-	long long  int value;
-	
-	value = ft_atoll(str);
-	if(!str || !*str)
-		return(false);
-	if (str[0] == '+' || str[0] == '-')
-		i++;
-	while(str && str[i])
-	{
-		if (!ft_isdigit(str[i]) && i > 0)
-			return(false);
-		i++;
-	}
-	if (value > (INT64_MAX) || (value) < (INT64_MIN))
-	{
-		printf("OKKKKKKKKK");
-		return (false);
-	}
-	return (true);
-}
+    long long nb = 0;
 
-int ft_exit(char **str) //prototypage qui sera changer par la suite
-{
-	long long int nb;
-
-	nb = 0;
-	if (str[1])
-	{
-		if (isnumber(str[1]) == true)
-		{
-			nb = ft_atoll(str[1]);
-			if (str[2])
-			{
-				printf("bash: exit: too many arguments\n");
-				return(-12);
-			}
-			return (nb);
-		}
-		printf("bash: exit: %s: numeric argument required\n", str[1]);
-		return (2);
-	}
-	return (errno);
+    if (args[1])
+    {
+        if (isnumber(args[1]))
+        {
+            nb = ft_atoll(args[1]);
+            if (args[2])
+            {
+                printf("bash: exit: too many arguments\n");
+                return (-12);
+            }
+            return (nb); // comportement bash : exit code = n % 256
+        }
+        printf("bash: exit: %s: numeric argument required\n", args[1]);
+        return (2);
+    }
+    return (errno); // ou 0 selon ton design
 }
 
 
