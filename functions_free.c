af98da94975c526c349e2b0b61db028588747358
William Da Silva Guerreiro <willda-s@z1r2p2.42lyon.fr>
2025-08-03 07:13:09 +0200

    repush content in functions_free.c

--- functions_free.c
+++ functions_free.c
@@ -1,3 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions_free.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: willda-s <willda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 07:10:30 by willda-s          #+#    #+#             */
+/*   Updated: 2025/08/03 07:11:08 by willda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "includes/parsing.h"
 
-void free_all
+void	free_tab(char **dst)
+{
+	int	i;
+
+	i = 0;
+	while (dst && dst[i])
+	{
+		free(dst[i]);
+		i++;
+	}
+	free(dst);
+}
+
+int	free_all(t_data *data, int errcode, char *str)
+{
+	if (data->env)
+		free_lst_env(&data->env, false, 0);
+	if (data->pars)
+		free_lst_pars(&data->pars);
+	if (data->exec)
+		free_lst_exec(&data->exec);
+	if (data->envp)
+		free_tab(data->envp);
+	if (data->dst)
+		free_tab(data->dst);
+	ft_putstr_fd(str, 2);
+	exit(errcode);
+}
