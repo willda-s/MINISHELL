66f4864b7df1ce2963a3ea0e7761f084b4e7bb50
Camille Fabre <cafabre@z4r3p2.42lyon.fr>
2025-08-28 18:18:02 +0200

    added the echo builtin - handling of option -n still to improve

---
+++ builtins/builtin_echo.c
@@ -0,0 +1,27 @@
+#include "builtins.h"
+
+int builtin_echo(t_exec *exec)
+{
+    int i;
+    int new_line;
+
+    i = 1;
+    new_line = 1;
+    //cas de l option -n
+    if (exec->cmd[1] && ft_strncmp(exec->cmd[1], "-n") == 0)
+    {
+        i = 2;
+        new_line = 0;
+    }
+    //parcourt des arguments de la commande
+    while (exec->cmd[i])
+    {
+        write(exec->fd_out, exec->cmd[1], ft_strlen(exec->cmd[1]));
+        if (exec->cmd[i + 1])
+            write(exec->fd_out, " ", 1);
+        i++;
+    }
+    if (new_line)
+        write(exec->fd_out, "\n", 1);
+    return (0);
+}
