5a106264c4e080a71168ea1b4fdca4fe6c0171b2
cafabre <camille.fabre003@gmail.com>
2025-08-28 13:57:16 +0200

    correction get output fd

--- builtins/builtins_utils.c
+++ builtins/builtins_utils.c
@@ -5,7 +5,7 @@ int get_output_fd(t_exec *exec)
     int output_fd;
 
     if (exec->fd_out == -1)
-        output_fd = STDOUT_FILENO;
+        output_fd = 1;
     else
         output_fd = exec->fd_out;
     return (output_fd);
