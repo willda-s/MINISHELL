59588b5d02962e72385c61e00860077ee9d64ea2
cafabre <camille.fabre003@gmail.com>
2025-08-28 14:47:10 +0200

    adding exit builtin - not completed

--- builtins/builtin_exit.c
+++ builtins/builtin_exit.c
@@ -37,3 +37,30 @@ long long ft_atoll(const char *str)
     }
     return (result * sign);
 }
+
+int builtin_exit(t_exec *exec)
+{
+   long long status;
+
+   write(1, "exit\n", 5);
+   if (exec->argc > 2)
+   {
+       write(1, "exit: too many arguments\n", 25);
+       return (1);
+   }
+   if (exec->argc == 2)
+   {
+       if (!is_number(exec->argv[1]))
+       {
+           write(1, "exit: numeric argument required\n", 32);
+           exit(2);
+       }
+       status = ft_atoll(exec->argv[1]);
+       exit((unsigned char)status);
+   }
+   exit(exec->last_status); //A completer
+}
+
+//last status : trouver un moyen de recuperer le code de la derniere commande
+//option 1 : ajouter a la structure exec
+//option 2 : var globale, bof...
