9e1ef18c73b73f1d769f9e7749d9097f382bd414
cafabre <camille.fabre003@gmail.com>
2025-08-31 13:54:02 +0200

    refactor: simplify builtin_pwd by removing unused output_fd handling

--- builtins/builtin_pwd.c
+++ builtins/builtin_pwd.c
@@ -5,15 +5,14 @@ int builtin_pwd(t_exec *exec)
     char    *cwd;
     int     output_fd;
 
-    output_fd = get_output_fd(exec);
     cwd = getcwd(NULL, 0);
     if (cwd == NULL)
     {
         perror("pwd");
         return (1);
     }
-    write(output_fd, cwd, ft_strlen(cwd));
-    write(output_fd, "\n", 1);
+    write(1, cwd, ft_strlen(cwd));
+    write(1, "\n", 1);
     free(cwd);
     return (0);
 }
