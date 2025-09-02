7ff8871dcdf4201f27b12f555468e724c5b582fd
cafabre <camille.fabre003@gmail.com>
2025-09-02 14:40:44 +0200

    fix error in echo builtin -> option -n handling

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -6,11 +6,30 @@
 /*   By: cafabre <cafabre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 14:07:42 by cafabre           #+#    #+#             */
-/*   Updated: 2025/08/31 20:45:50 by cafabre          ###   ########.fr       */
+/*   Updated: 2025/09/02 14:37:27 by cafabre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+
+static bool	check_n_option(char *s)
+{
+	int	j;
+
+	j = 0;
+	if (s[j] != '-')
+		return (false);
+	j++;
+	if (!s[j] || s[j] != 'n')
+		return (false);
+	while (s[j])
+	{
+		if (s[j] != 'n')
+			return (false);
+		j++;
+	}
+	return (true);
+}
 
 int	builtin_echo(t_exec *exec)
 {
@@ -19,7 +38,7 @@ int	builtin_echo(t_exec *exec)
 
 	i = 1;
 	new_line = 1;
-	if (exec->cmd[i] && ft_strncmp(exec->cmd[i], "-n", 2) == 0)
+	while (exec->cmd[i] && check_n_option(exec->cmd[i]) == true)
 	{
 		i++;
 		new_line = 0;
