78fb46ba17610b12d2e281812ad3b6152a576f42
cafabre <camille.fabre003@gmail.com>
2025-09-25 20:19:52 +0200

    fix builtins norm

--- builtins/builtin_env.c
+++ builtins/builtin_env.c
@@ -6,12 +6,24 @@
 /*   By: cafabre <camille.fabre003@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 14:07:51 by cafabre           #+#    #+#             */
-/*   Updated: 2025/09/25 20:03:03 by cafabre          ###   ########.fr       */
+/*   Updated: 2025/09/25 20:18:42 by cafabre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "libft.h"
+
+static void	display_empty_export(t_env *tmp)
+{
+	write(1, "export ", 7);
+	write(1, tmp->key, ft_strlen(tmp->key));
+	if (ft_strlen(tmp->value) >= 1)
+	{
+		write(1, "=", 1);
+		write(1, tmp->value, ft_strlen(tmp->value));
+	}
+	write(1, "\n", 1);
+}
 
 int	builtin_env(t_data *data, bool export)
 {
@@ -21,16 +33,7 @@ int	builtin_env(t_data *data, bool export)
 	while (tmp)
 	{
 		if (export == true)
-		{
-			write(1, "export ", 7);
-			write(1, tmp->key, ft_strlen(tmp->key));
-			if (ft_strlen(tmp->value) >= 1)
-			{
-				write(1, "=", 1);
-				write(1, tmp->value, ft_strlen(tmp->value));
-			}
-			write(1, "\n", 1);
-		}
+			display_empty_export(tmp);
 		else if (ft_strlen(tmp->value) >= 1)
 		{
 			write(1, tmp->key, ft_strlen(tmp->key));
