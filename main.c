2473ca4fe712b23d9f2b1bd9f4aa012595db5705
akarapkh <pacan.vedenskiy@gmail.com>
2025-09-29 22:33:21 +0200

    fixed leak

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akarapkh <akarapkh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 23:43:26 by willda-s          #+#    #+#             */
-/*   Updated: 2025/09/28 03:03:35 by akarapkh         ###   ########.fr       */
+/*   Updated: 2025/09/29 22:31:34 by akarapkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,72 +137,6 @@ static int	handle_exec(t_data *data)
 	return (0);
 }
 
-// int	main(int ac, char **av, char **env)
-// {
-// 	char	**dst;
-// 	t_data	data;
-
-// 	ft_bzero(&data, sizeof(t_data));
-// 	(void)ac;
-// 	(void)av;
-// 	data.env = NULL;
-// 	if (ac == 1)
-// 	{
-// 		init_lst_env(&data.env, env);
-// 		setup_main_signals();
-// 		while (1)
-// 		{
-// 			data.syntax_error_flag = 0;
-// 			data.input = readline("minishell> ");
-// 			if (!data.input)
-// 			{
-// 				ft_dprintf(2, "exit\n");
-// 				rl_clear_history();
-// 				free_all(&data, true, 12);
-// 			}
-// 			if (*data.input)
-// 				add_history(data.input);
-// 			data.input = check_input(data.input);
-// 			if (!data.input)
-// 			{
-// 				free_lst_env(&data.env, false, 0);
-// 				continue ;
-// 			}
-// 			dst = ft_split_with_quotes(data.input, ' ');
-// 			if (!dst)
-// 			{
-// 				free(data.input);
-// 				free_lst_env(&data.env, true, 0);
-// 			}
-// 			init_data(&data, &data.env, dst);
-// 			token_main(&data);
-// 			if (data.pars && validate_syntax(data.pars))
-// 			{
-// 				g_signal_status = 2;
-// 				free_tmpall(&data);
-// 				continue ;
-// 			}
-// 			init_lst_exec(&data);
-// 			if (expand_exec_list(&data) != 0)
-// 				continue ;
-// 			init_envp(&data);
-// 			// remove_empty_line(&data);
-// 			// print_lst_exec(data.exec);
-// 			// print_lst_pars(data.pars);
-// 			handle_heredoc(&data);
-// 			execc(&data);
-// 			free_tmpall(&data);
-// 		}
-// 	}
-// 	return (data.errcode);
-// }
-
-/*TO DO :
-			- expand vide dans le cas du token=commands alors pas d'execution
-			- handle builtins dans exec
-			- $? et code de sortie
-			- signal
-*/
 
 /*A SUPPRIMER DU .H ET DU .C :
 			- print_lst_env(t_env *envd);
