df9b26eaf734fe7049dbfaaf50ecf06e6c5f3a1f
willda-s <william.guerreiro2003@gmail.com>
2025-09-30 02:49:15 +0200

    fix norm 1

--- exec/exec_utils.c
+++ exec/exec_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cafabre <camille.fabre003@gmail.com>       +#+  +:+       +#+        */
+/*   By: willda-s <willda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 22:47:42 by willda-s          #+#    #+#             */
-/*   Updated: 2025/09/28 22:54:18 by cafabre          ###   ########.fr       */
+/*   Updated: 2025/09/30 02:48:11 by willda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 #include "signals.h"
 #include <stdbool.h>
 #include <sys/wait.h>
+
+static	int	exec_builtins_bis(t_exec *node, t_data *data)
+{
+	int	val;
+
+	if (node->cmd && ft_strcmp(node->cmd[0], "pwd") == 0)
+		val = builtin_pwd();
+	else if (node->cmd && ft_strcmp(node->cmd[0], "env") == 0)
+		val = builtin_env(data, false);
+	else if (node->cmd && ft_strcmp(node->cmd[0], "cd") == 0)
+		val = builtin_cd(node, data);
+	else if (node->cmd && ft_strcmp(node->cmd[0], "export") == 0)
+		val = builtin_export(node, data);
+	else if (node->cmd && ft_strcmp(node->cmd[0], "unset") == 0)
+		val = builtin_unset(node, data);
+	return (val);
+}
 
 bool	exec_builtins(t_exec *node, t_data *data, int fd_backup_in,
 		int fd_backup_out)
@@ -37,16 +54,8 @@ bool	exec_builtins(t_exec *node, t_data *data, int fd_backup_in,
 		val = builtin_exit(node, data);
 		val = 0;
 	}
-	else if (node->cmd && ft_strcmp(node->cmd[0], "pwd") == 0)
-		val = builtin_pwd();
-	else if (node->cmd && ft_strcmp(node->cmd[0], "env") == 0)
-		val = builtin_env(data, false);
-	else if (node->cmd && ft_strcmp(node->cmd[0], "cd") == 0)
-		val = builtin_cd(node, data);
-	else if (node->cmd && ft_strcmp(node->cmd[0], "export") == 0)
-		val = builtin_export(node, data);
-	else if (node->cmd && ft_strcmp(node->cmd[0], "unset") == 0)
-		val = builtin_unset(node, data);
+	else
+		val = exec_builtins_bis(node, data);
 	return (val);
 }
 
